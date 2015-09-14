e395ff746ce4cbcf7b1488354dddbb92f4f37dfd
Matt Chatterley <matt@code.je>
2015-09-14 15:22:35 +0100

    Initial commit - empty Visual Studio project

---
+++ scorecard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="scorecard.MvcApplication" Language="C#" %>
---
+++ scorecard/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(scorecard.Startup))]
+namespace scorecard
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
