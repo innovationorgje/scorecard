710c905863e2bdd0f555ee8ea3a7a6e2ee5d34b7
Matt Chatterley <matt@code.je>
2015-09-15 15:30:41 +0100

    Strip unnecessary/unused actions/views/etc - clean it up a bit so it's just a 'very warm mess', not a hot one.

--- scorecard/Controllers/HomeController.cs
+++ scorecard/Controllers/HomeController.cs
@@ -19,19 +19,5 @@ namespace scorecard.Controllers
             
             return View(model);
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
