7878a2e011cc096a5a4aad4f27c53e1b0c48a439
Matt Chatterley <matt@code.je>
2015-09-15 12:29:53 +0100

    Tidy up (a bit). Still a hot mess.

---
+++ scorecard/Models/HomeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace scorecard.Models
+{
+    public class HomeIndexViewModel
+    {
+        public List<Group> Groups { get; set; }
+    }
+}
